f48ed1867c301ced94dc3710dba698777019d2ba
DESKTOP-GTH902A\HansolJu <hansol1108@naver.com>
2018-01-06 14:10:13 +0900

    14page

--- c++tuto/c++tuto/HelloWorld.cpp
+++ c++tuto/c++tuto/HelloWorld.cpp
@@ -1,4 +1,9 @@
-#include<stdio.h>
-int main() {
-	printf("HelloWorld");
+#include<iostream>
+
+int main(void) {
+	int num = 20;
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello " << "World!" << std::endl;
+	std::cout << num << ' ' << 'A', std::cout << ' ' << 3.14 << std::endl;
+	return 0;
 }
