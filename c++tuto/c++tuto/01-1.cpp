4a4eba9b27a88855675a0b9f205fd070acada00d
DESKTOP-GTH902A\HansolJu <hansol1108@naver.com>
2018-01-06 17:23:03 +0900

    01-1_1

---
+++ c++tuto/c++tuto/01-1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+int main() {
+	int val[5];
+	int result=0;
+
+	for (int i = 0; i < 5; i++) {
+		std::cout << i+1 << "번째 정수 입력: ";
+		std::cin >> val[i];
+		result += val[i];
+	}
+	std::cout << "합계: " << result << std::endl;
+}
