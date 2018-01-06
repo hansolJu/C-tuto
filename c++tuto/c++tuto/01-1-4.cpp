c898d5cd9251d7459bbbd18a03514e65a6e85c4f
DESKTOP-GTH902A\HansolJu <hansol1108@naver.com>
2018-01-06 21:04:25 +0900

    01-1-4

---
+++ c++tuto/c++tuto/01-1-4.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+//int main() {
+//	int input = 0;
+//	while (1) {
+//		std::cout << "판매금액을 만원 단위로 입력(-1 to end): ";
+//		std::cin >> input;
+//		if (input == -1) {
+//			std::cout << "프로그램을 종료합니다." << std::endl;
+//			break;
+//		}
+//		else {
+//			std::cout << "이번달 급여: " << 50 + (input*0.12) << "만원" << std::endl;
+//		}
+//	}
+//}
+int CalSalary(int sales) {
+	return (int)(50 + sales * 0.12);
+}
+int main() {
+	int sales;
+	while (1) {
+		std::cout << "판매금액을 만원 단위로 입력(-1 to end): ";
+		std::cin >> sales;
+		if (sales == -1) {
+			std::cout << "프로그램을 종료합니다." << std::endl;
+			break;
+		}
+		else {
+			std::cout << "이번달 급여: " << CalSalary(sales) << "만원" << std::endl;
+		}
+	}
+	return 0;
+}
