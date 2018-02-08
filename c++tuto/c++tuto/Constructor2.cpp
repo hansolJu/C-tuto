f063026b7c7cd4de99ca6a14f901137b4662a457
DESKTOP-GTH902A\HansolJu <hansol1108@naver.com>
2018-02-09 04:49:24 +0900

    173p 이전 예제에 대한 생성자의 활용

---
+++ c++tuto/c++tuto/Constructor2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class SimpleClass
+{
+public:
+	SimpleClass(int n1 = 0, int n2 = 0) {
+		num1 = n1;
+		num2 = n2;
+	}
+	void ShowData()const {
+		cout << num1 << " " << num2 << endl;
+	}
+
+private:
+	int num1;
+	int num2;
+
+};
+
+int main() {
+	SimpleClass sc1();
+	SimpleClass mysc = sc1();
+	mysc.ShowData();
+	return 0;
+}
+
+SimpleClass sc1() {
+	SimpleClass sc(20, 30);
+	return sc;
+}
