604be9e9a1e23bcac11a43c5deec21957a4aec69
DESKTOP-GTH902A\HansolJu <hansol1108@naver.com>
2018-02-24 17:12:15 +0900

    254p c언어에서 이야기한 static

---
+++ c++tuto/c++tuto/CComstatic.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void Counter() {
+	static int cnt;// static 변수는 전역변수와 마찬가지로 초기화하지 않으면 0으로 초기화된다. 
+	//그리고 이 문장은 딱 한번 실행이 된다. 즉, cnt는 counter함수가 호출될 때마다 새롭게 할당되는 지역변수가 아니다.
+	cnt++;
+	cout << "Current cnt: " << cnt << endl;
+}
+
+int main() {
+	for (int i = 0; i < 10; i++) {
+		Counter();
+	}
+	return 0;
+}
