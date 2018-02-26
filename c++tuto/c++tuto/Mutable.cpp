461b80e3bb2544623a423627f82cdfcf5110096f
DESKTOP-GTH902A\HansolJu <hansol1108@naver.com>
2018-02-26 13:38:42 +0900

    키워드  mutable

---
+++ c++tuto/c++tuto/Mutable.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class SoSimple {
+private:
+	int num1;
+	mutable int num2; //const 함수에 대해 예외를 둠
+public:
+	SoSimple(int n1, int n2)
+		:num1(n1), num2(n2)
+	{}
+	void ShowSimpleData() const {
+		cout << num1 << ", " << num2 << endl;
+	}
+	void CopyToNum2() const {
+		num2 = num1;//num2가 mutable로 선언되었기 때문에 가능하다.
+	}
+};
+
+int main() {
+	SoSimple sm(1, 2);
+	sm.ShowSimpleData();
+	sm.CopyToNum2();
+	sm.ShowSimpleData();
+	return 0;
+}
